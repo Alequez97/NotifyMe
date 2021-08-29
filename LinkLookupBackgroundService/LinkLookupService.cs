577e425d8f37cb5a6b9bbaeeef964505ffe7b1bb
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-08-29 11:40:48 +0300

    Add and rename background worker

---
+++ LinkLookupBackgroundService/LinkLookupService.cs
@@ -0,0 +1,58 @@
+using LinkLookup;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinkLookupBackgroundService
+{
+    public class LinkLookupService : BackgroundService
+    {
+        private readonly ILinkLookup _linkLookup;
+        private IEnumerable<string> _links = new List<string>()
+        {
+            "https://habr.com/ru/flows/develop/"
+        };
+        private HttpClient _httpClient;
+
+        public LinkLookupService(ILinkLookup linkLookup)
+        {
+            _linkLookup = linkLookup;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _httpClient = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                foreach (var link in _links)
+                {
+                    var html = await _httpClient.GetStringAsync(link);
+                    var allLinksInPage = _linkLookup.GetAllLinks(html);
+                    allLinksInPage.Insert(0, "\n*****************");
+                    allLinksInPage.Insert(0, DateTime.Now.ToString());
+
+                    File.WriteAllLines(@"C:\Tmp\links.txt", allLinksInPage);
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
+            }
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _httpClient.Dispose();
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
--- LinkLookupBackgroundService/Program.cs
+++ LinkLookupBackgroundService/Program.cs
@@ -1,24 +1,27 @@
+using LinkLookup;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace NotificationBackgroundService
+namespace LinkLookupBackgroundService
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddHostedService<Worker>();
+                    services.AddTransient<ILinkLookup, RegexLinkLookup>();
+                    services.AddHostedService<LinkLookupService>();
                 });
     }
 }
