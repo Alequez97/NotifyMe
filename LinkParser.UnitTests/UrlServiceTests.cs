84b10ab11353507a425ca7b0b695e8c82398e8a1
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-09-24 21:39:38 +0300

    Add url service unit tests

---
+++ LinkParser.UnitTests/UrlServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using LinkLookup.Models;
+using LinkLookup.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkLookup.UnitTests
+{
+    public class UrlServiceTests
+    {
+        private class MockLinkLookup : ILinkLookup
+        {
+            public List<Url> GetAllLinks(string text)
+            {
+                return new List<Url>();
+            }
+        }
+
+        private List<Url> _mockDownloadedLinks;
+        private UrlService _urlService;
+        private List<Url> _links;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockDownloadedLinks = new List<Url>()
+            {
+                new Url("http://www.w3.com"),
+                new Url("https://www.google.com/test"),
+                new Url("https://translate.yandex.ru"),
+                new Url("/relative/path")
+            };
+            _urlService = new UrlService(new MockLinkLookup());
+            _links = new List<Url>()
+            {
+                new Url("https://www.google.com")
+            };
+        }
+
+        [Test]
+        public void UrlServiceTest()
+        {
+            var expectedList = new List<Url>()
+            {
+                new Url("https://www.google.com/test"),
+                new Url("https://www.google.com/relative/path"),
+            };
+
+            List<Url> downloadedLinks = new List<Url>();
+            foreach (var link in _links)
+            {
+                downloadedLinks = _urlService.RemoveAlienLinks(_mockDownloadedLinks, link);
+                downloadedLinks = _urlService.ConcatenateRelativeLinksWithHost(downloadedLinks, link);
+            }
+
+            Assert.AreEqual(expectedList, downloadedLinks);
+        }
+    }
+}
