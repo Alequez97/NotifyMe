647d900ed4e656a0a2f5f10d2ca22bc01c4c2642
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-09-27 22:14:53 +0300

    Register logger and config reader in DI container

--- LinkLookupBackgroundService/Program.cs
+++ LinkLookupBackgroundService/Program.cs
@@ -1,7 +1,11 @@
+using CommonUtils.ConfigReader;
+using CommonUtils.Logging;
 using LinkLookup;
 using LinkLookup.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace LinkLookupBackgroundService
 {
@@ -12,14 +16,27 @@ namespace LinkLookupBackgroundService
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseWindowsService()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddTransient<UrlService>();
-                    services.AddTransient<ILinkLookup, HtmlAntlrLinkLookup>();
-                    services.AddHostedService<LinkLookupService>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.Local.json", optional: true)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
+                    .UseWindowsService()
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services.AddTransient<UrlService>();
+                        services.AddTransient<IConfigReader, JsonConfigReader>();
+                        services.AddTransient<ILogger>(x => new TextFileLogger($"{hostContext.Configuration.GetValue<string>("Workspace")}/Logs"));
+                        services.AddTransient<ILinkLookup, HtmlAntlrLinkLookup>();
+                        services.AddHostedService<LinkLookupService>();
+                    });
+        }
+            
     }
 }
