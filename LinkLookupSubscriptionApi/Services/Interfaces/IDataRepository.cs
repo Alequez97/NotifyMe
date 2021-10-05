60cecb1a02bcfad4796e2b4b28b72d79beac4632
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-10-05 09:15:49 +0300

    Add xml comments in data repository inteface

--- LinkLookupSubscriptionApi/Services/Interfaces/IDataRepository.cs
+++ LinkLookupSubscriptionApi/Services/Interfaces/IDataRepository.cs
@@ -1,21 +1,51 @@
-﻿using System;
+﻿using LinkLookupSubscriptionApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace LinkLookupSubscriptionApi.Services.Interfaces
 {
-    public interface IDataRepository<T> where T : new()
+    public interface IDataRepository<T> where T : ModelBase, new()
     {
+        /// <summary>
+        /// Gets object by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>null if object not found. Otherwise return object</returns>
         T Read(string id);
 
+        /// <summary>
+        /// Get all model records
+        /// </summary>
+        /// <returns>null if no records was found. Otherwise return list of objects</returns>
         List<T> ReadAll();
 
+        /// <summary>
+        /// Creates new record
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>true if object was saved. Otherwise false</returns>
         bool Write(T obj);
 
+        /// <summary>
+        /// Creates existing record
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>true if object was updated. Otherwise false</returns>
         bool Update(T obj);
 
+        /// <summary>
+        /// Delete existing record
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>true if object was deleted. Otherwise false</returns>
         bool Delete(string id);
 
+        /// <summary>
+        /// Find object by linq-type expressions
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Records that matches condition in expression</returns>
         T FindByExpression(Expression<Func<T, bool>> expression);
     }
 }
