43828d3098d2b6230ab3aff80ad08bb582ab9a46
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-10-17 00:38:55 +0300

    Rename controllers variables

--- LinkLookupSubscriptionApi/Controllers/GroupsController.cs
+++ LinkLookupSubscriptionApi/Controllers/GroupsController.cs
@@ -12,56 +12,56 @@ namespace LinkLookupSubscriptionApi.Controllers
     [ApiController]
     public class GroupsController : ControllerBase
     {
-        private IDataRepository<Group> _usersDataRepository;
+        private IDataRepository<Group> _groupsDataRepository;
         private readonly IMapper _mapper;
 
         public GroupsController(IDataRepositoryFactory dataRepositoryFactory, IMapper mapper)
         {
-            _usersDataRepository = dataRepositoryFactory.Get<Group>("Groups");
+            _groupsDataRepository = dataRepositoryFactory.Get<Group>("Groups");
             _mapper = mapper;
         }
-
+        
         // GET: api/<ValuesController>
         [HttpGet]
         public ActionResult<List<GroupDto>> Get()
         {
-            var users = _usersDataRepository.ReadAll();
-            var usersDto = _mapper.Map<List<GroupDto>>(users);
-            return usersDto != null ? Ok(usersDto) : NotFound();
+            var groups = _groupsDataRepository.ReadAll();
+            var groupsDto = _mapper.Map<List<GroupDto>>(groups);
+            return groupsDto != null ? Ok(groupsDto) : NotFound();
         }
 
         //GET api/<ValuesController>/5
         [HttpGet("{id}")]
         public ActionResult<GroupDto> Get(string id)
         {
-            var user = _usersDataRepository.Read(id);
-            var userDto = _mapper.Map<GroupDto>(user);
-            return userDto != null ? Ok(userDto) : NotFound();
+            var group = _groupsDataRepository.Read(id);
+            var groupDto = _mapper.Map<GroupDto>(group);
+            return groupDto != null ? Ok(groupDto) : NotFound();
         }
 
         [HttpGet("get-by-username/{username}")]
-        public ActionResult<GroupDto> GetByUsername(string username)
+        public ActionResult<GroupDto> GetByUsername(string groupName)
         {
-            var user = _usersDataRepository.FindByExpression(x => x.Name == username);
-            var userDto = _mapper.Map<GroupDto>(user);
-            return userDto != null ? Ok(userDto) : NotFound();
+            var group = _groupsDataRepository.FindByExpression(x => x.Name == groupName);
+            var groupDto = _mapper.Map<GroupDto>(group);
+            return groupDto != null ? Ok(groupDto) : NotFound();
         }
 
         // POST api/<ValuesController>
         [HttpPost]
-        public IActionResult Post([FromBody] GroupDto userDto)
+        public IActionResult Post([FromBody] GroupDto groupDto)
         {
-            var user = _mapper.Map<Group>(userDto);
-            var isSuccess = _usersDataRepository.Write(user);
+            var group = _mapper.Map<Group>(groupDto);
+            var isSuccess = _groupsDataRepository.Write(group);
             return isSuccess ? Ok() : new JsonResult(new Response() { Message = "Failed to save user" }) { StatusCode = StatusCodes.Status500InternalServerError };
         }
 
         //PUT api/<ValuesController>
         [HttpPut]
-        public IActionResult Put([FromBody] GroupDto userDto)
+        public IActionResult Put([FromBody] GroupDto groupDto)
         {
-            var user = _mapper.Map<Group>(userDto);
-            var isSuccess = _usersDataRepository.Update(user);
+            var group = _mapper.Map<Group>(groupDto);
+            var isSuccess = _groupsDataRepository.Update(group);
             return isSuccess ? Ok() : new JsonResult(new Response() { Message = "Failed to update user" } ) { StatusCode = StatusCodes.Status500InternalServerError };
         }
 
@@ -69,7 +69,7 @@ namespace LinkLookupSubscriptionApi.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(string id)
         {
-            var isSuccess = _usersDataRepository.Delete(id);
+            var isSuccess = _groupsDataRepository.Delete(id);
             return isSuccess ? Ok() : new JsonResult(new Response() { Message = "Failed to delete user" } ) { StatusCode = StatusCodes.Status500InternalServerError };
         }
     }
