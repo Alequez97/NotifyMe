43112833b904cee25669a33f8e396cbb5c06814a
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-10-18 21:33:32 +0300

    Add mongo db configuration reader in link lookup background service

---
+++ LinkLookupBackgroundService/ConfigurationReaders/MongoDbLinkLookupConfigReader.cs
@@ -0,0 +1,47 @@
+﻿using DomainEntites;
+using LinkLookupBackgroundService.Interfaces;
+using MessageSender.Models;
+using MongoDB.Driver;
+using System;
+
+namespace LinkLookupBackgroundService.ConfigurationReaders
+{
+    public class MongoDbLinkLookupConfigReader : ILinkLookupConfigReader
+    {
+        private readonly IMongoCollection<Group> _collection;
+        private readonly string _groupName;
+
+        /// <summary>
+        /// Constructor that connects to mongoDb by connection string
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="databaseName"></param>
+        /// <param name="tableName"></param>
+        public MongoDbLinkLookupConfigReader(string groupName, string connectionString, string databaseName = "NotifyMe", string tableName = "Groups")
+        {
+            var client = new MongoClient(connectionString);
+            var mongoDb = client.GetDatabase(databaseName);
+            _groupName = groupName;
+            _collection = mongoDb.GetCollection<Group>(tableName);
+        }
+
+        /// <summary>
+        /// Constructor that connects to localhost mongoDb
+        /// </summary>
+        /// <param name="databaseName"></param>
+        /// <param name="tableName"></param>
+        public MongoDbLinkLookupConfigReader(string groupName, string databaseName = "NotifyMe", string tableName = "Groups")
+        {
+            var client = new MongoClient();
+            var mongoDb = client.GetDatabase(databaseName);
+            _groupName = groupName;
+            _collection = mongoDb.GetCollection<Group>(tableName);
+        }
+
+        public NotifyConfig GetGroupsConfiguration()
+        {
+            var group = _collection.Find(g => g.Name == _groupName).FirstOrDefault();
+            return group?.NotifyConfig ?? throw new InvalidOperationException($"Group with name {_groupName} not found");
+        }
+    }
+}
