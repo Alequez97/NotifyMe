a2eac99e65874396c05c58c4be476ff69e027f0e
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-06-07 14:10:44 +0300

    Remove unused F# project

---
+++ TestingHttpStuff/Program.fs
@@ -0,0 +1,19 @@
+﻿open System.Net.Http
+open System.IO
+open System.Threading
+
+let downloadGooglePageAsync = 
+    async {
+        use client = new HttpClient()
+        let! page = client.GetStringAsync("https://www.google.com/search?q=Sanja") |> Async.AwaitTask
+        do! File.WriteAllTextAsync("C:/Tmp/newAsyncPage.html", page) |> Async.AwaitTask
+        printfn "Google page was downloaded"
+    }
+
+[<EntryPoint>]
+let main argv =
+    Async.Start downloadGooglePageAsync
+    while true do 
+        printf "%c" '*'
+        Thread.Sleep(100)
+    0
